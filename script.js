868e3d4e5d0a57ee3e0230b8cf5c0eb30642c4e5
unknown <ngonibejoseph@gmail.com>
2026-08-11 10:52:04 +0000

    add home page image

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('button');
-
-    if (button) {
-        button.addEventListener('click', () => {
-            window.location.href = 'lessons.html';
-        });
-    }
-});
